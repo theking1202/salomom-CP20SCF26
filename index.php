f025515430f5009094022d88d6dbd2b4651b21da
STUDENT <loinguyen030@gmail.com>
2020-11-23 19:27:01 +0700

    cập nhật index

--- index.php
+++ index.php
@@ -5,12 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+        .box{
+            width: 200px;
+            height:200px;
+            background: red;
+        }
+    </style>
 </head>
 <body>
     <?php
-        echo '<h1 style="color:red">Hello word!!</h1>';
+
+        $hoten = 'Lợi';
+        echo '<h1 style="color:red">Xin chào </h1>';
     ?>
-    <a href="pages/lienhe.php">Liên hệ</a><br>
-    <a href="pages/gioithieu.php">Giới thiệu</a>
+    <div class="box">
+        <h1>Xin chào <?= $hoten?></h1>
+    </div>
+    <a href="pages/lienhe.php" style="text-decoration:none">Liên hệ</a><br>
+    <a href="pages/gioithieu.php" style="text-decoration:none">Giới thiệu</a>
 </body>
 </html>
